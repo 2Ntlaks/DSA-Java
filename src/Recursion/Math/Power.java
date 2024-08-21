08906125431a12ee322711dffa3e13285eab9a31
Ntlakanipho Mgaguli <133015962+2Ntlaks@users.noreply.github.com>
2024-08-21 10:04:41 +0200

    Update Power.java
    Just written the code, what do you think?

--- src/Recursion/Math/Power.java
+++ src/Recursion/Math/Power.java
@@ -1,8 +1,18 @@
 package Recursion.Math;
 
 public class Power {
+  public int pow(int m,n)
+    {
+        if(n == 0)
+            return 1;
+        else
+            return m*pow(m,n-1);
+    }
+    
     public static void main(String[] args) {
+              int results = pow(5,2);
 
+        System.out.println(results);
     }
 
 
